32f9f375f8102a2a4e36b1c0c2c8a18319e9231f
houwenbo <manbok.hau@gmail.com>
2022-02-24 22:29:28 +0800

    Modify the sample Vue pages

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,3 +1,27 @@
 <template>
-    <h1>Home</h1>
+    <Head>
+        <title>{{ $page.props.title }} - My awesome app</title>
+    </Head>
+
+    <div class="p-6">
+        <div class="flex space-x-4 mb-4">
+            <Link
+                :href="route('home')"
+                class="text-gray-700 bg-gray-200 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >Home</Link
+            >
+            <Link
+                :href="route('about')"
+                class="text-gray-700 bg-gray-200 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >About</Link
+            >
+            <Link
+                :href="route('contact')"
+                class="text-gray-700 bg-gray-200 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >Contact</Link
+            >
+        </div>
+
+        <h1>This is: {{ $page.props.title }}</h1>
+    </div>
 </template>
