544a293b192dae35f6d6b31043d461c5b5e961c2
houwenbo <manbok.hau@gmail.com>
2022-02-24 21:50:54 +0800

    Setting up app.js bugfix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,29 +1,6 @@
 const path = require("path");
 const mix = require("laravel-mix");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        // require TailwindCSS
-        require("tailwindcss")
-    ]);
-
-/*
- |--------------------------------------------------------------------------
- | Ziggy Setup
- |--------------------------------------------------------------------------
- */
-
 // Rezolve Ziggy
 mix.alias({
     ziggy: path.resolve("vendor/tightenco/ziggy/dist/vue"),
