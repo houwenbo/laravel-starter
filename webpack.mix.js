c678a06cf30ba29ba3d3eed4d35da2d66b8660b0
houwenbo <manbok.hau@gmail.com>
2022-02-24 21:48:22 +0800

    Setting up app.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
-const mix = require('laravel-mix');
+const path = require("path");
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -22,9 +23,22 @@ mix.js('resources/js/app.js', 'public/js')
  | Ziggy Setup
  |--------------------------------------------------------------------------
  */
-const path = require("path");
 
 // Rezolve Ziggy
 mix.alias({
     ziggy: path.resolve("vendor/tightenco/ziggy/dist/vue"),
 });
+
+// Build files
+mix.js("resources/js/app.js", "public/js")
+    .vue({ version: 3 })
+    .webpackConfig({
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "resources/js"),
+            },
+        },
+    })
+    .extract()
+    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
+    .version();
